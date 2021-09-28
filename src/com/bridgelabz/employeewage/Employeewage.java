5ff12dd155523b5b0ae983a736ed6cff0d9f5abf
Naveen Pattanashetti <pattanashettinaveen.com>
2021-09-28 18:52:24 +0530

    Added :program to check employee is present or absent

--- src/com/bridgelabz/employeewage/Employeewage.java
+++ src/com/bridgelabz/employeewage/Employeewage.java
@@ -3,8 +3,15 @@ package com.bridgelabz.employeewage;
 public class Employeewage {
 
 	public static void main(String[] args) {
-	System.out.println("welcome to employee wage program");
-
-	}
+		System.out.println("Welcome to Employee Wage Computation program !! ");
+		int is_present = 1;
+		int emp_check = (int)(Math.floor(Math.random() * 10)) % 2;
+		if( emp_check == is_present)
+		{
+			System.out.println("Employee is present");
+		}
+		else
+			System.out.println("Employee is absent");
+}
 
 }
