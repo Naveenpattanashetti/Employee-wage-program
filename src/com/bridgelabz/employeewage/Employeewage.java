302863c67add3a86fde28050dd4e1aec9f78ba35
Naveen Pattanashetti <pattanashettinaveen.com>
2021-09-28 18:47:57 +0530

    modification in employee wage program

--- src/com/bridgelabz/employeewage/Employeewage.java
+++ src/com/bridgelabz/employeewage/Employeewage.java
@@ -3,7 +3,7 @@ package com.bridgelabz.employeewage;
 public class Employeewage {
 
 	public static void main(String[] args) {
-	System.out.println("welcome to employee wage program ");
+	System.out.println("welcome to employee wage program");
 
 	}
 
