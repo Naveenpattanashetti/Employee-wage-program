f870c29b112c4719d58c188a951a5a811747212d
raj <rajashekhar.pattar@talentpod.com>
2021-09-28 18:31:23 +0530

    welcome to employee wage program

---
+++ src/com/bridgelabz/employeewage/Employeewage.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.employeewage;
+
+public class Employeewage {
+
+	public static void main(String[] args) {
+	System.out.println("welcome to employee wage program");
+
+	}
+
+}
